ae7348d2324c4883469f1bf727745acbc64a5df6
gitbush-test <82594147+megi-j@users.noreply.github.com>
2023-01-20 13:31:39 +0400

    feat: add video and play buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import play from "./images/play-button.png";
+import left from "./images/left.png";
+import right from "./images/fast-forward.png";
+const videoUrl =
+  "https://upload.wikimedia.org/wikipedia/commons/transcoded/f/f3/Big_Buck_Bunny_first_23_seconds_1080p.ogv/Big_Buck_Bunny_first_23_seconds_1080p.ogv.1080p.vp9.webm";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <video controls>
+        <source src={videoUrl} type="video/webm" />
+      </video>
+      <PlayBox>
+        <img style={{ width: 32, height: 32 }} src={left} alt="" />
+        <img src={play} alt="play-button" />
+        <img style={{ width: 32, height: 32 }} src={right} alt="" />
+      </PlayBox>
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  max-width: 1440px;
+  width: 100%;
+`;
+const PlayBox = styled.div`
+  width: 300px;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`;
