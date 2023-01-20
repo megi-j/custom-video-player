d02bd1a7658f4f9e2af4d0c4353e7e836f3d1e0b
gitbush-test <82594147+megi-j@users.noreply.github.com>
2023-01-20 15:23:44 +0400

    feat: add video start and pause functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,29 @@ import styled from "styled-components";
 import play from "./images/play-button.png";
 import left from "./images/left.png";
 import right from "./images/fast-forward.png";
+import { useRef } from "react";
 const videoUrl =
   "https://upload.wikimedia.org/wikipedia/commons/transcoded/f/f3/Big_Buck_Bunny_first_23_seconds_1080p.ogv/Big_Buck_Bunny_first_23_seconds_1080p.ogv.1080p.vp9.webm";
 
 function App() {
+  const videoRef = useRef<any>(null);
+
   return (
     <Container>
-      <video controls>
+      <video ref={videoRef}>
         <source src={videoUrl} type="video/webm" />
       </video>
       <PlayBox>
         <img style={{ width: 32, height: 32 }} src={left} alt="" />
-        <img src={play} alt="play-button" />
+        <img
+          src={play}
+          alt="play-button"
+          onClick={() =>
+            videoRef.current.paused
+              ? videoRef.current.play()
+              : videoRef.current.pause()
+          }
+        />
         <img style={{ width: 32, height: 32 }} src={right} alt="" />
       </PlayBox>
     </Container>
